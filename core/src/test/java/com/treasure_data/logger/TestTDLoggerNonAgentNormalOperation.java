870e3dcd140cdc0cb93a94e3420929b1210da38b
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-06-30 17:50:57 +0900

    minor change

--- core/src/test/java/com/treasure_data/logger/TestTDLoggerNonAgentNormalOperation.java
+++ core/src/test/java/com/treasure_data/logger/TestTDLoggerNonAgentNormalOperation.java
@@ -16,43 +16,22 @@ public class TestTDLoggerNonAgentNormalOperation {
         props.load(this.getClass().getClassLoader().getResourceAsStream("treasure-data.properties"));
     }
 
-    @Test @Ignore
+    @Test
     public void testNormal01() throws Exception {
         Properties props = System.getProperties();
         props.setProperty(Config.TD_LOGGER_AGENTMODE, "false");
         TreasureDataLogger logger = TreasureDataLogger.getLogger("mugadb");
 
-        for (int i = 0; i < 10; ++i) {
+        //long baseTime = System.currentTimeMillis() / 1000 / 3600 * 3600;
+        long baseTime = 1363939200;
+        for (int i = 0; i < 100; ++i) {
             Map<String, Object> data = new HashMap<String, Object>();
-            for (int j = 0; j < 16; ++j) {
-                data.put("kk:" + ":" + j, "vv:" + ":" + j);
-            }
-            logger.log("mugatbl", data);
+            data.put("time", baseTime + (i * 60));
+            data.put("k:" + i, "v:" + i);
+            logger.log("testtbl", data);
         }
 
-        //logger.flush();
-        //TreasureDataLogger.closeAll();
-        
-        Thread.sleep(100 * 1000);
+        logger.close();
     }
 
-    @Test @Ignore
-    public void testNormal02() throws Exception {
-        Properties props = System.getProperties();
-        props.setProperty(Config.TD_LOGGER_AGENTMODE, "false");
-        TreasureDataLogger logger = TreasureDataLogger.getLogger("mugadb");
-
-        for (int i = 0; i < 1000; ++i) {
-            Map<String, Object> data = new HashMap<String, Object>();
-            for (int j = 0; j < 16; ++j) {
-                data.put("kk:" + ":" + j, "vv:" + ":" + j);
-            }
-            logger.log("newscore01", data);
-        }
-
-        //logger.flush();
-        //TreasureDataLogger.closeAll();
-        
-        Thread.sleep(100 * 1000);
-    }
 }
